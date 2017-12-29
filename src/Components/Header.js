441651a9de7f2efc1af6c3c96e20b94f4cc4e804
ArtemZV <zakharchukartem@gmail.com>
2017-12-29 14:39:36 +0200

    S-2 redirect to main page correction

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
-function redirectToMainPage(){
-    window.location = "http://localhost:3000/";
-}
-
 class Header extends React.Component{
+    redirectToMainPage = () => {
+        window.location = MAIN_PAGE_URL;
+    }
+
     render(){
         const {account} = this.props;
         return(
             <div>
-                <span className="logo" onClick={redirectToMainPage}><img src="/devico_001.svg" alt="logo"/></span>
+                <span className="logo" onClick={this.redirectToMainPage}>
+                    <img src="/devico_001.svg" alt="logo"/>
+                </span>
                 <span className="accountBlock">
-                    <span className="accountName">{account.firstName} {account.lastName}</span>
-                    <span className="accountAvatar"><i className="material-icons">account_circle</i></span>
+                    <span className="accountName">
+                        {account.firstName} {account.lastName}
+                    </span>
+                    <span className="accountAvatar">
+                        <i className="material-icons">account_circle</i>
+                    </span>
                 </span>
             </div>
         )
