9164927f9d9df54c3794058703a9d4bf5b14bf54
ArtemZV <zakharchukartem@gmail.com>
2018-01-02 15:34:36 +0200

    Header correction

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,19 +5,19 @@ class Header extends React.Component{
         const {account} = this.props;
         return(
             <div>
-                <span className="logo">
+                <div className="logoBlock">
                     <a href="/" title="Main Page">
                         <img src="/devico_001.svg" alt="logo"/>
                     </a>
-                </span>
-                <span className="accountBlock">
+                </div>
+                <div className="accountBlock">
                     <span className="accountName">
                         {account.firstName} {account.lastName}
                     </span>
                     <span className="accountAvatar">
                         <i className="material-icons">account_circle</i>
                     </span>
-                </span>
+                </div>
             </div>
         )
     }
