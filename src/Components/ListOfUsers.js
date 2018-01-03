65abfe2f42241212dcca12194fe16412e82a7134
ArtemZV <zakharchukartem@gmail.com>
2018-01-03 13:25:45 +0200

    Split ListOfUsers on components

--- src/Components/ListOfUsers.js
+++ src/Components/ListOfUsers.js
@@ -1,56 +1,7 @@
 import React from 'react';
+import User from './User'
+
 import './ListOfUsers.scss';
-
-class ContextMenu extends React.Component{
-    render(){
-        const {userId, activeContextMenu, onOpenReviewFrom, onUserDelete} = this.props;
-        return(
-            <div className={activeContextMenu === userId ? "userContextMenu open" : "userContextMenu"}>
-                <div className="contextMenuBody">
-                    <div onClick={() => onOpenReviewFrom(userId)}>Add review</div>
-                    <div onClick={() => onUserDelete(userId)}>Delete user</div>
-                </div>
-            </div>
-        );
-    }
-}
-
-class User extends React.Component{
-    render(){
-        const {
-            user,
-            onUserContextMenu,
-            activeContextMenu,
-            activeUserId,
-            onShowUserProfile,
-            onOpenReviewFrom,
-            onUserDelete} = this.props;
-
-        return(
-            <li>
-                <div className="userListItem">
-                    <span className={activeUserId !== user.id ? "user" : "user active"} onClick={() => onShowUserProfile(user.id)}>
-                        <span className="userIcon" >
-                            <i className="material-icons">person</i>
-                        </span>
-                        <span className="userName">
-                            {user.firstName} {user.lastName}
-                        </span>
-                    </span>
-                    <span className={activeContextMenu === user.id ? "userContextMenuBtn active" : "userContextMenuBtn"} onClick={() => onUserContextMenu(user.id)}>
-                        <i className="material-icons">more_vert</i>
-                    </span>
-                </div>
-                <ContextMenu
-                    userId={user.id}
-                    activeContextMenu={activeContextMenu}
-                    onOpenReviewFrom={onOpenReviewFrom}
-                    onUserDelete={onUserDelete}
-                />
-            </li>
-        )
-    }
-}
 
 class ListOfUsers extends React.Component{
     state = {
