3db4d25aa84e5fb163b57a4c2ac81905961dc014
ArtemZV <zakharchukartem@gmail.com>
2018-01-04 12:51:57 +0200

    B-1 bugs fixes: delete unnecessary files, change server start config, update App, update styles, little improves

--- src/App.js
+++ src/App.js
@@ -1,104 +1,39 @@
 import React, { Component } from 'react';
-import UserForm from './Components/UserForm';
-import UsersTable from './Components/UsersTable';
-import ReviewForm from './Components/ReviewForm';
-import PopupsBlock from './Components/PopupsBlock'
+import Header from './Components/Header';
+import PopupsBlock from './Components/PopupsBlock';
+import './App.scss';
 
 var popupId = 1;
 
 const usersArr = [
-  {firstName: "Test", lastName: "User", id: 1, isAdmin: false},
-  {firstName: "Test", lastName: "User 2", id: 2, isAdmin: false},
-  {firstName: "Admin", lastName: "User", id: 3, isAdmin: true}
+  {firstName: "Test", lastName: "User", id: 1, isAdmin: false, skills: ["React", "Koa"]},
+  {firstName: "Test", lastName: "User 2", id: 2, isAdmin: false, skills: ["Angular", "Express"]},
+  {firstName: "Admin", lastName: "User", id: 3, isAdmin: true, skills: ["React, Redux"]}
 ]
 
 const reviewsArr = [
-  {reviewText: 'test review 1', id: 1, userId:usersArr[0].id, isAproved: true},
-  {reviewText: 'test review 2', id: 2, userId:usersArr[0].id, isAproved: true},
-  {reviewText: 'test review 3', id: 3, userId:usersArr[1].id, isAproved: true},
-  {reviewText: 'test review 4', id: 4, userId:usersArr[1].id, isAproved: true},
-  {reviewText: 'default review', id: 5, userId:usersArr[2].id, isAproved: true}
+  {reviewText: 'test review 1', id: 1, userId:usersArr[0].id, date: Date.now() - 100*60*1000},
+  {reviewText: 'test review 2', id: 2, userId:usersArr[0].id, date: Date.now()},
+  {reviewText: 'test review 3', id: 3, userId:usersArr[1].id, date: Date.now() - 100*60*1000},
+  {reviewText: 'test review 4', id: 4, userId:usersArr[1].id, date: Date.now()},
+  {reviewText: 'default review', id: 5, userId:usersArr[2].id, date: Date.now()}
 ]
+
+const admin = usersArr.find(user => user.isAdmin === true);
 
 class App extends Component {
   state = {
     users: usersArr,
     reviews: reviewsArr,
-    editingUser: null,
     popups: []
   };
 
-  handleUserFormSubmit = (user) => {
-    let {popups, users, editingUser} = this.state;
-    if (editingUser){
-      users = users.map((usr) => {
-        if (usr.id === user.id){
-          return user;
-        }
-        return usr;
-      });
-      popups.push({message: "User updated", id: popupId++});
-    }
-    else {
-      users.push(user)
-      popups.push({message: "New user created", id: popupId++});
-    }
-    this.setState({editingUser: null, popups, users});
-  }
-
-  handleReviewCreated = (review) => {
-    const {reviews, popups} = this.state;
-    reviews.push(review);
-    popups.push({message: "New review created", id: popupId++});
-    this.setState({review, popups});
-  }
-
-  handleDeleteUser = (user) => {
-    let {reviews, users, popups} = this.state;
-
-    user.reviews.forEach((review) => {reviews = reviews.map((rev) => {if (rev.id !== review.id) return rev})});
-    users = users.filter((usr) => usr.id !== user.id);
-    popups.push({message: "User deleted", id: popupId++});
-
-    this.setState({popups, users, reviews});
-  }
-
-  handleDeleteReview = (review) => {
-    let {reviews, popups} = this.state;
-    reviews = reviews.filter((rev) => rev.id !== review.id);
-    popups.push({message: "Review deleted", id: popupId++});
-    this.setState({reviews, popups});
-  }
-
-  handlePopupClose = (id) => {
-    let {popups} = this.state;
-    popups = popups.filter((popup) => popup.id !== id);
-    this.setState({popups});
-  }
-
-  handleUserEdit = (editingUser) => {
-    this.setState({editingUser});
-  }
-
   render() {
-    const {editingUser, users, reviews, popups} = this.state;
+    const {popups} = this.state;
+
     return (
       <div className="App">
-        <div className="inlineBlock">
-          <UserForm editingUser={editingUser} onUserFormSubmit={this.handleUserFormSubmit} />
-        </div>
-        <div className="inlineBlock">
-          <ReviewForm users={users} onReviewCreate={this.handleReviewCreated} />
-        </div>
-        <div className="wideBlock">
-          <UsersTable
-            users={users}
-            reviews={reviews}
-            onUserDelete={this.handleDeleteUser}
-            onReviewDelete={this.handleDeleteReview}
-            onUserEdit={this.handleUserEdit}
-          />
-        </div>
+        <Header account={admin}/>
         <PopupsBlock popups={popups} onPopupClose={this.handlePopupClose}/>
       </div>
     );
--- src/Components/PopupsBlock.js
+++ src/Components/PopupsBlock.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './PopupsBlock.scss';
 
 class Popup extends React.Component {
     state = {
